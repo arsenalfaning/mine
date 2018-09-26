2dbfba25f54a12fc73a1b69fdc2a404f0771e4be
arsenalfaning <765205025@qq.com>
2018-09-26 12:18:30 +0800

    v0.451

--- src/main/java/com/flower/mine/repository/GainRepository.java
+++ src/main/java/com/flower/mine/repository/GainRepository.java
@@ -9,6 +9,6 @@ import java.util.Date;
 import java.util.List;
 
 public interface GainRepository extends PagingAndSortingRepository<Gain, Gain.GainPK> {
-    @Query(value = "SELECT DATE_FORMAT(date, '%Y-%m-%d'), value FROM gain WHERE username = :username AND date >= :start  AND date <= :end", nativeQuery = true)
+    @Query(value = "SELECT DATE_FORMAT(date, '%Y-%m-%d'), SUM(value) FROM gain WHERE username = :username AND date >= :start  AND date <= :end GROUP BY date", nativeQuery = true)
     List<Object[]> findAllByUsernameAndDate(@Param("username") String username, @Param("start") Date start, @Param("end") Date end);
 }
